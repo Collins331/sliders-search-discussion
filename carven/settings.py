18e8e770520a16f4cecdbd4cd16f74c3b4c72b13
Collins <ochiengcollinsoduor2@gmail.com>
2024-11-21 14:53:55 +0300

    update settings

--- carven/settings.py
+++ carven/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7+h13p$q6@j!*066=oygvylb)317_^hl+c1j6$2ytj=)ro^4de'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
